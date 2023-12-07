117ea27ad861205d436711003d2fa9b7aee46cb5
Gleicimar <gleicimarribeiro@gmail.com>
2023-12-07 18:45:55 -0300

    Add files via upload
    adicionado o inicio do site

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+require('vendor/autoload.php');
+
+date_default_timezone_set('America/Sao_Paulo');
+
+define('INCLUDE_PATH','http://localhost/projetos/Reaprendendo/');
+define('INCLUDE_PATH_STATIC',INCLUDE_PATH.'App/Views/pages/');
+
+$app = new App\Application();
+$app->run();
